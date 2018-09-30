07743d5933f6f26f09738595fbcbd8787afa1302
Dmitry Mersiyanov <dmersiyanov@yandex.ru>
2018-09-30 14:53:44 +0300

    added email send intent

--- app/src/main/java/com/dmity/androidacademy/AboutActivity.kt
+++ app/src/main/java/com/dmity/androidacademy/AboutActivity.kt
@@ -1,12 +1,49 @@
 package com.dmity.androidacademy
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_about.*
+
 
 class AboutActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        btn_send.setOnClickListener { composeEmail(email_msg.text.toString()) }
+        btn_telegram.setOnClickListener { openTelegram() }
+
     }
+
+    private fun composeEmail(message: String, email: Array<String> = arrayOf("dmersiyanov@yandex.ru"), subject: String = "Деловое предложение") {
+        val intent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:")
+            putExtra(Intent.EXTRA_EMAIL, email)
+            putExtra(Intent.EXTRA_SUBJECT, subject)
+            putExtra(Intent.EXTRA_TEXT, message)
+        }
+
+        if(intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        } else
+            Toast.makeText(this, getString(R.string.error_no_email_app), Toast.LENGTH_SHORT).show()
+
+    }
+
+    private fun openTelegram() {
+        try {
+            val telegramIntent = Intent(Intent.ACTION_VIEW)
+            telegramIntent.data = Uri.parse("http://telegram.me/dmersiyanov")
+            startActivity(telegramIntent)
+
+        } catch (e: Exception) {
+            Toast.makeText(this, getString(R.string.error_message_send), Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
 }
