de48428b4e4315e3edb81561e3cd5da1b01be450
Dmitry Mersiyanov <dmersiyanov@yandex.ru>
2018-11-07 09:08:10 +0300

    fixes for ViewModel

--- app/src/main/java/com/dmity/androidacademy/MainActivity.kt
+++ app/src/main/java/com/dmity/androidacademy/MainActivity.kt
@@ -2,10 +2,9 @@ package com.dmity.androidacademy
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import androidx.lifecycle.LiveDataReactiveStreams
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -17,13 +16,8 @@ import com.dmity.androidacademy.models.GenericNewsItem
 import com.dmity.androidacademy.utils.isPortrait
 import com.dmity.androidacademy.utils.visible
 import com.dmity.androidacademy.viewModel.NewsViewModel
-import com.google.android.material.snackbar.Snackbar
-import io.reactivex.Flowable
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.view_error_stub.*
-import java.util.concurrent.TimeUnit
 
 class MainActivity : BaseActivity() {
 
@@ -42,11 +36,15 @@ class MainActivity : BaseActivity() {
 
     override fun initUi() {
         initRecycler()
-        getNews()
+//        getNews()
+
+        observeNews()
+
+
     }
 
     override fun initUx() {
-        btnRetry.setOnClickListener { getNews() }
+        btnRetry.setOnClickListener { viewModel.getNews() }
     }
 
     override fun showProgress(show: Boolean) {
@@ -76,27 +74,33 @@ class MainActivity : BaseActivity() {
         }
     }
 
-    private fun getNews() {
-        Flowable.fromPublisher(LiveDataReactiveStreams.toPublisher(this, viewModel.getNews()))
-                .firstOrError()
-                .subscribeOn(Schedulers.io())
-                .delay(DELAY, TimeUnit.SECONDS)
-                .observeOn(AndroidSchedulers.mainThread())
-                .doOnSubscribe {
-                    showProgress(true)
-                    showError("", false)
-                }
-                .subscribe({
-                    adapter.items = it
-                    showProgress(false)
-                }, {
-                    Log.e(TAG, it.message)
-                    it.printStackTrace()
-                    showError("", true)
-                    showProgress(false)
-                    Snackbar.make(rvNews, getString(R.string.error_loading), Snackbar.LENGTH_LONG).show()
-                })
-                .bind()
+//    private fun getNews() {
+//        Flowable.fromPublisher(LiveDataReactiveStreams.toPublisher(this, viewModel.getNews()))
+//                .firstOrError()
+//                .subscribeOn(Schedulers.io())
+//                .delay(DELAY, TimeUnit.SECONDS)
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .doOnSubscribe {
+//                    showProgress(true)
+//                    showError("", false)
+//                }
+//                .subscribe({
+//                    adapter.items = it
+//                    showProgress(false)
+//                }, {
+//                    Log.e(TAG, it.message)
+//                    it.printStackTrace()
+//                    showError("", true)
+//                    showProgress(false)
+//                    Snackbar.make(rvNews, getString(R.string.error_loading), Snackbar.LENGTH_LONG).show()
+//                })
+//                .bind()
+//    }
+
+    private fun observeNews() {
+        viewModel.news.observe(this, Observer {
+            adapter.items = it
+        })
     }
 
     private fun initRecycler() {
--- app/src/main/java/com/dmity/androidacademy/viewModel/NewsViewModel.kt
+++ app/src/main/java/com/dmity/androidacademy/viewModel/NewsViewModel.kt
@@ -1,20 +1,70 @@
 package com.dmity.androidacademy.viewModel
 
-import androidx.lifecycle.LiveData
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.dmity.androidacademy.base.SubscriptionsHolder
 import com.dmity.androidacademy.models.DisplayableItem
 import com.dmity.androidacademy.utils.DataUtils
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
-class NewsViewModel: ViewModel() {
+class NewsViewModel: ViewModel(), SubscriptionsHolder {
 
-    private var news: MutableLiveData<List<DisplayableItem>> = MutableLiveData()
+    override val disposables: CompositeDisposable = CompositeDisposable()
 
-    fun getNews(): LiveData<List<DisplayableItem>> {
-        if (news.value == null) {
-            news.value = DataUtils.generateNews()
-        }
-        return news
+    var news: MutableLiveData<List<DisplayableItem>> = MutableLiveData()
+    val showProgress = MutableLiveData<Boolean>()
+    val showError = MutableLiveData<Boolean>()
+
+
+    init {
+        getNews()
+    }
+
+    fun getNews() {
+        Single.fromCallable { DataUtils.generateNews()  }
+                .subscribeOn(Schedulers.io())
+//                .delay(DELAY, TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe {
+                    //                    showProgress(true)
+//                    showError("", false)
+                }
+                .subscribe({
+
+                    news.value = it
+//                    adapter.items = it
+//                    showProgress(false)
+                }, {
+                    Log.e(TAG, it.message)
+                    it.printStackTrace()
+//                    showError("", true)
+//                    showProgress(false)
+//                    Snackbar.make(rvNews, getString(R.string.error_loading), Snackbar.LENGTH_LONG).show()
+                })
+                .bind()
+    }
+
+
+
+//    fun getNews(): LiveData<List<DisplayableItem>> {
+//        if (news.value == null) {
+//            news.value = DataUtils.generateNews()
+//        }
+//        return news
+//    }
+
+    override fun onCleared() {
+        resetCompositeDisposable()
+    }
+
+
+    companion object {
+        private const val DELAY = 2L
+        private const val TAG = "NewsViewModel"
     }
 
 }
