8d5d36df634636b7ba6bddcb13be2f9d9c1ed6b5
dmersiyanov <dmersiyanov@yandex.ru>
2020-01-09 21:03:39 +0700

    fixed error with wrong app class

--- base/core/src/main/java/com/dmity/androidacademy/core/testing/TestApplication.kt
+++ base/core/src/main/java/com/dmity/androidacademy/core/testing/TestApplication.kt
@@ -1,11 +1,23 @@
 package com.dmity.androidacademy.core.testing
 
 import android.app.Application
-import com.dmity.androidacademy.core.R
+import android.content.Context
+import com.dmity.androidacademy.core.di.AppWithFacade
+import com.dmity.androidacademy.core.di.ProvidersFacade
+import com.dmity.androidacademy.core.utils.AndroidPlatformProxyImpl
+import com.dmity.androidacademy.domain.system.AndroidPlatformProxy
 
-class TestApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        setTheme(R.style.Theme_AppCompat) //or just R.style.Theme_AppCompat
+class TestApplication : Application(), AppWithFacade {
+
+    override fun getFacade(): ProvidersFacade {
+        return object : ProvidersFacade {
+            override fun provideContext(): Context {
+                return this@TestApplication
+            }
+
+            override fun provideAndroidPlatformProxy(): AndroidPlatformProxy {
+                return AndroidPlatformProxyImpl(this@TestApplication)
+            }
+        }
     }
 }
